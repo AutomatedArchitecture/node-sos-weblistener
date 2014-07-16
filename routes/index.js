6f88ab8cefccd55b9062679df05ddfdae2d0235d
lprichar <github@leerichardson.com>
2014-07-16 01:19:14 -0400

    Try to connect to the sos device on loading of the index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var sos = require('sos-device');
+
+function getSosDeviceInfo(callback, startupData) {
+    return startupData.sosDevice.readAllInfo(function (err, deviceInfo) {
+        if (err) {
+            return callback(err);
+        }
+        console.log("deviceInfo:", deviceInfo);
+        return callback(null, deviceInfo);
+    });
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Siren of Shame Web Listener' });
+    sos.connect(function(err, sosDevice) {
+        if (err) {
+            res.render('index', { title: 'Siren of Shame Web Listener', device: null });
+        }
+        sosDevice.readAllInfo(function(err2, deviceInfo) {
+            if (err2) {
+                res.render('index', { title: 'Siren of Shame Web Listener', device: null });
+            }
+            console.log("deviceInfo:", deviceInfo);
+            res.render('index', { title: 'Siren of Shame Web Listener', device: sosDevice, deviceInfo: deviceInfo });
+        });
+    });
 });
 
 router.post('/turnon', function (req, res) {
