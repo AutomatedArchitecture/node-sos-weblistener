dbdf70fa94f2067ee68d78ede4862232431396cf
Lee Richardson <github@leerichardson.com>
2014-07-16 06:49:30 +0000

    lights up the siren when you click the buttongit add .

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,31 @@ router.get('/', function(req, res) {
 
 router.post('/turnon', function (req, res) {
     console.log('Turn on the siren!');
-    res.render('index', { title: 'Siren of Shame Web Listener' });
+    sos.connect(function(err, sosDevice) {
+        if (err) {
+            res.render('index', { title: 'Siren of Shame Web Listener', err: err });
+        }
+        sosDevice.readAllInfo(function(err2, deviceInfo) {
+            if (err2) {
+                res.render('index', { title: 'Siren of Shame Web Listener', err: err2 });
+            } else {
+                var controlPacket = {
+                    audioMode: deviceInfo.audioPatterns[0].id,
+                    audioPlayDuration: 500,
+                    ledMode: deviceInfo.ledPatterns[0].id,
+                    ledPlayDuration: 500
+                };
+                console.log(controlPacket);
+                sosDevice.sendControlPacket(controlPacket, function(err3) {
+                    if (err3) {
+                        res.render('index', { title: 'Siren of Shame Web Listener', err: err3 });
+                    } else {
+                        res.render('index', { title: 'Siren of Shame Web Listener', device: sosDevice, deviceInfo: deviceInfo });
+                    }
+                });
+            }
+        });
+    });
 });
 
 module.exports = router;
