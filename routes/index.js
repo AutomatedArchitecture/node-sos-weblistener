a112cacb903cd27f42b2615c003ebc51a4fcff74
lprichar <github@leerichardson.com>
2014-07-16 00:45:56 -0400

    Ability to post back to server-side and handle button clicks

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Siren of Shame Web Listener' });
+});
+
+router.post('/turnon', function (req, res) {
+    console.log('Turn on the siren!');
+    res.render('index', { title: 'Siren of Shame Web Listener' });
 });
 
 module.exports = router;
